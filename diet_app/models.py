b582a106d9f65e2ece1a6c56229abf97ba2e2365
cezarydepta <cezdep@gmail.com>
2017-12-08 19:32:19 +0100

    Add __str__ to models

--- diet_app/models.py
+++ diet_app/models.py
@@ -16,10 +16,16 @@ class Weight(models.Model):
     value = models.FloatField()
     date = models.DateField()
 
+    def __str__(self):
+        return str(self.date) + ' ' + str(self.value)
+
 
 class Diary(models.Model):
     user_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
     date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
 
 
 class Meal(models.Model):
@@ -29,18 +35,27 @@ class Meal(models.Model):
     total_proteins = models.FloatField(null=True, blank=True)
     total_fat = models.FloatField(null=True, blank=True)
 
+    def __str__(self):
+        return str(self.diary_id)
+
 
 class MealType(models.Model):
     meal_id = models.ForeignKey(Meal, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
-    kcal = models.FloatField(null=True, blank=True)
-    carbs = models.FloatField(null=True, blank=True)
-    proteins = models.FloatField(null=True, blank=True)
-    fat = models.FloatField(null=True, blank=True)
+    kcal = models.FloatField()
+    carbs = models.FloatField()
+    proteins = models.FloatField()
+    fat = models.FloatField()
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -48,13 +63,22 @@ class Ingredient(models.Model):
     meal_id = models.ForeignKey(Meal, null=True, on_delete=models.CASCADE)
     amount = models.FloatField()
 
+    def __str__(self):
+        return str(self.product_id)
+
 
 class Discipline(models.Model):
     name = models.CharField(max_length=30)
     calories_burn = models.FloatField()
+
+    def __str__(self):
+        return self.name
 
 
 class Activity(models.Model):
     diary_id = models.ForeignKey(Diary, on_delete=models.CASCADE)
     discipline_id = models.ForeignKey(Discipline, on_delete=models.CASCADE)
     time = models.TimeField()
+
+    def __str__(self):
+        return str(self.discipline_id)
